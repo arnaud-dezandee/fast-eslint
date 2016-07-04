82ee38af7b9e6eb4c140e9550c54ed9c65515263
Arnaud Dezandee <dezandee.arnaud@gmail.com>
2016-07-04 11:49:12 +0200

    chore(package): update atom-linter to version 6.0.0
    use rangeFromLineNumber to get column, cleanup specs

--- spec/linter-spec.js
+++ spec/linter-spec.js
@@ -29,65 +29,57 @@ describe('Fast-ESLint provider for Linter', () => {
   it('finds something wrong with airbnb/bad.js', () => {
     openFile('fixtures/airbnb/bad.js', (messages) => {
       expect(messages.length).toEqual(5);
-      [
-        { range: [[0, 0], [0, 16]], type: 'Error' },
-        { range: [[1, 0], [1, 13]], type: 'Error' },
-        { range: [[3, 0], [3, 25]], type: 'Error' },
-        { range: [[3, 7], [3, 25]], type: 'Error' },
+      const expected = [
+        { range: [[0, 0], [0, 3]], type: 'Error' },
+        { range: [[1, 0], [1, 3]], type: 'Error' },
+        { range: [[3, 0], [3, 2]], type: 'Error' },
+        { range: [[3, 7], [3, 13]], type: 'Error' },
         { range: [[3, 25], [3, 25]], type: 'Error' },
-      ].forEach((expected, index) => {
-        expect(messages[index].type).toEqual(expected.type);
-        expect(messages[index].range).toEqual(expected.range);
-      });
+      ];
+      expect(messages.map(({ range, type }) => ({ range, type }))).toEqual(expected);
     });
   });
 
   it('finds something wrong with google/bad.js', () => {
     openFile('fixtures/google/bad.js', (messages) => {
       expect(messages.length).toEqual(4);
-      [
-        { range: [[3, 0], [3, 25]], type: 'Error' },
-        { range: [[3, 7], [3, 25]], type: 'Error' },
+      const expected = [
+        { range: [[3, 0], [3, 2]], type: 'Error' },
+        { range: [[3, 7], [3, 13]], type: 'Error' },
         { range: [[3, 25], [3, 25]], type: 'Error' },
-        { range: [[5, 0], [5, 19]], type: 'Warning' },
-      ].forEach((expected, index) => {
-        expect(messages[index].type).toEqual(expected.type);
-        expect(messages[index].range).toEqual(expected.range);
-      });
+        { range: [[5, 0], [5, 8]], type: 'Warning' },
+      ];
+      expect(messages.map(({ range, type }) => ({ range, type }))).toEqual(expected);
     });
   });
 
   it('finds something wrong with standard/bad.js', () => {
     openFile('fixtures/standard/bad.js', (messages) => {
       expect(messages.length).toEqual(7);
-      [
+      const expected = [
         { range: [[0, 15], [0, 16]], type: 'Error' },
         { range: [[1, 12], [1, 13]], type: 'Error' },
-        { range: [[3, 0], [3, 25]], type: 'Error' },
-        { range: [[3, 7], [3, 25]], type: 'Error' },
+        { range: [[3, 0], [3, 2]], type: 'Error' },
+        { range: [[3, 7], [3, 13]], type: 'Error' },
         { range: [[5, 15], [5, 19]], type: 'Error' },
         { range: [[6, 18], [6, 19]], type: 'Error' },
         { range: [[9, 8], [9, 9]], type: 'Error' },
-      ].forEach((expected, index) => {
-        expect(messages[index].type).toEqual(expected.type);
-        expect(messages[index].range).toEqual(expected.range);
-      });
+      ];
+      expect(messages.map(({ range, type }) => ({ range, type }))).toEqual(expected);
     });
   });
 
   it('finds something wrong with xo/bad.js', () => {
     openFile('fixtures/xo/bad.js', (messages) => {
       expect(messages.length).toEqual(5);
-      [
-        { range: [[3, 0], [3, 25]], type: 'Error' },
-        { range: [[3, 0], [3, 25]], type: 'Error' },
-        { range: [[3, 7], [3, 25]], type: 'Error' },
+      const expected = [
+        { range: [[3, 0], [3, 2]], type: 'Error' },
+        { range: [[3, 0], [3, 2]], type: 'Error' },
+        { range: [[3, 7], [3, 13]], type: 'Error' },
         { range: [[3, 25], [3, 25]], type: 'Error' },
-        { range: [[6, 2], [6, 19]], type: 'Error' },
-      ].forEach((expected, index) => {
-        expect(messages[index].type).toEqual(expected.type);
-        expect(messages[index].range).toEqual(expected.range);
-      });
+        { range: [[6, 2], [6, 8]], type: 'Error' },
+      ];
+      expect(messages.map(({ range, type }) => ({ range, type }))).toEqual(expected);
     });
   });
 
