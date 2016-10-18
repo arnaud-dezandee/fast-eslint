f85c8333da23a195f7d9e7914c91f2e5ad975970
Arnaud Dezandee <dezandee.arnaud@gmail.com>
2016-10-18 12:23:12 +0200

    test(airbnb): refactor specs to check all fields

--- spec/linter-spec.js
+++ spec/linter-spec.js
@@ -5,48 +5,98 @@
  */
 
 import path from 'path';
+
 process.chdir('/');
+
+/**
+ * Privates
+ */
+
+const lint = require('../lib/index.js').provideLinter().lint;
+
+const openFile = filePath => (
+  atom.workspace
+    .open(filePath)
+    .then(editor => lint(editor))
+);
+
+const htmlMsg = (ruleId, message) => (
+  `<span class="badge badge-flexible">${ruleId || 'Fatal'}</span> ${message}`
+);
 
 /**
  * Tests
  */
 
 describe('Fast-ESLint provider for Linter', () => {
-  const lint = require('../lib/index.js').provideLinter().lint;
-
-  const openFile = (fileName, cb) => (
-    waitsForPromise(() => (
-      atom.workspace
-        .open(path.join(__dirname, fileName))
-        .then(editor => lint(editor).then(cb))
-    ))
+  beforeEach(() =>
+    waitsForPromise(() => atom.packages.activatePackage('fast-eslint'))
   );
 
-  beforeEach(() => {
-    waitsForPromise(() => atom.packages.activatePackage('fast-eslint'));
-  });
+  describe('finds something wrong with airbnb/bad.js', () => {
+    const filePath = path.join(__dirname, 'fixtures/airbnb/bad.js');
+    const expectedRuleIds = [
+      'no-var', 'keyword-spacing', 'eqeqeq',
+      'no-undef', 'semi', 'no-multiple-empty-lines',
+      'no-unused-expressions', 'no-undef', 'semi',
+    ];
+    const expectedRanges = [
+      { start: { row: 0, column: 0 }, end: { row: 0, column: 3 } },
+      { start: { row: 2, column: 0 }, end: { row: 2, column: 2 } },
+      { start: { row: 2, column: 7 }, end: { row: 2, column: 13 } },
+      { start: { row: 2, column: 10 }, end: { row: 2, column: 13 } },
+      { start: { row: 2, column: 25 }, end: { row: 2, column: 25 } },
+      { start: { row: 7, column: 0 }, end: { row: 7, column: 0 } },
+      { start: { row: 10, column: 0 }, end: { row: 10, column: 1 } },
+      { start: { row: 10, column: 0 }, end: { row: 10, column: 1 } },
+      { start: { row: 10, column: 1 }, end: { row: 10, column: 1 } },
+    ];
+    const expectedMessages = [
+      'Unexpected var, use let or const instead.',
+      'Expected space(s) after "if".',
+      'Expected \'===\' and instead saw \'==\'.',
+      '\'bar\' is not defined.',
+      'Missing semicolon.',
+      'More than 2 blank lines not allowed.',
+      'Expected an assignment or function call and instead saw an expression.',
+      '\'a\' is not defined.',
+      'Missing semicolon.',
+    ];
+    let results = [];
 
-  it('finds something wrong with airbnb/bad.js', () => {
-    openFile('fixtures/airbnb/bad.js', (messages) => {
-      expect(messages.length).toEqual(9);
-      const expected = [
-        { range: [[0, 0], [0, 3]], type: 'Error' },
-        { range: [[1, 0], [1, 3]], type: 'Error' },
-        { range: [[3, 0], [3, 2]], type: 'Error' },
-        { range: [[3, 7], [3, 13]], type: 'Error' },
-        { range: [[3, 25], [3, 25]], type: 'Error' },
-        { range: [[10, 0], [10, 0]], type: 'Error' },
-        { range: [[11, 0], [11, 1]], type: 'Error' },
-        { range: [[11, 0], [11, 1]], type: 'Error' },
-        { range: [[11, 1], [11, 1]], type: 'Error' },
-      ];
-      expect(messages.map(({ range, type }) => ({ range, type }))).toEqual(expected);
+    beforeEach(() =>
+      waitsForPromise(() =>
+        openFile(filePath).then(messages => (results = messages))
+      )
+    );
+
+    it('messages length', () => {
+      expect(results.length).toEqual(9);
+    });
+
+    expectedRuleIds.forEach((rule, idx) => {
+      it(rule, () => {
+        const { filePath: file, range, type, html } = results[idx];
+        expect(file).toEqual(filePath);
+        expect(range).toEqual(expectedRanges[idx]);
+        expect(type).toEqual('Error');
+        expect(html).toEqual(htmlMsg(expectedRuleIds[idx], expectedMessages[idx]));
+      });
     });
   });
 
-  it('reports nothing with ignored ignore/bad.js', () => {
-    openFile('fixtures/ignore/bad.js', (messages) => {
-      expect(messages.length).toEqual(0);
+  describe('finds nothing with ignored ignore/bad.js', () => {
+    const filePath = path.join(__dirname, 'fixtures/ignore/bad.js');
+    let results = [];
+
+    beforeEach(() =>
+      waitsForPromise(() =>
+        openFile(filePath).then(messages => (results = messages))
+      )
+    );
+
+    it('ignored', () => {
+      expect(results.length).toEqual(0);
     });
   });
 });
